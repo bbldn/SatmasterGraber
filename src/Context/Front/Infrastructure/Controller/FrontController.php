55acf9430fb0bde7adfe97955d683250e1564d3b
bbldn05 <bobol.boris@gmail.com>
2021-09-01 15:34:15 +0300

    01.09.2021 15:34

---
+++ src/Context/Front/Infrastructure/Controller/FrontController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Context\Front\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FrontController extends AbstractController
+{
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -11,25 +11,33 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    /**
+     * @param ContainerConfigurator $container
+     * @return void
+     */
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+        if (true === is_file(dirname(__DIR__).'/config/services.yaml')) {
             $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+            $container->import('../config/{services}_' . $this->environment . '.yaml');
         } else {
             $container->import('../config/{services}.php');
         }
     }
 
+    /**
+     * @param RoutingConfigurator $routes
+     * @return void
+     */
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (true === is_file(dirname(__DIR__) .' /config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
         } else {
             $routes->import('../config/{routes}.php');
